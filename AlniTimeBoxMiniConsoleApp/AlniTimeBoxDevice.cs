cc421357902d0d350a6460296a70723b71e87559
Alexander Nilsen <alexander.nilsen.no@gmail.com>
2018-12-14 10:59:26 +0100

    Add initial code + Update TimeboxCSharp submodule
    * Add "AlniTimeBoxDevice" class - extends the "TimeBoxDevice" class 
      from the "Knom.TimeBox" project from the TimeboxCSharp submodule
        * Add functions to manage and show pixel art (array of colors) on 
          the device - based on the "ShowImage()" and "ConvertImage()" 
          functions from the base class
    * Add a link to the "ByteHelper" class to main project - needed by the
      functions of the extended "AlniTimeBoxDevice" class
    * Update the TimeboxCSharp submodule to make it easier to extend the
      "TimeBoxDevice" class

---
+++ AlniTimeBoxMiniConsoleApp/AlniTimeBoxDevice.cs
@@ -0,0 +1,72 @@
+﻿using Knom.TimeBox;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlniTimeBoxMiniConsoleApp
+{
+    class AlniTimeBoxDevice : TimeBoxDevice
+    {
+        public static byte[] ConvertColors(Color[][] colors)
+        {
+            var imageBytes = new List<byte>();
+            imageBytes.Add(0);
+
+            int counter = 0;
+
+            bool uneven = true;
+            for (int y = 0; y < colors.Length; y++)
+            {
+                for (int x = 0; x < colors[y].Length; x++)
+                {
+                    // get the color of the particular pixel of the image
+                    var color = colors[y][x];
+
+                    // Alpha channel < 32 --> drawn as black - "transparent"
+                    if (color.A < 32)
+                        color = Color.Black;
+
+                    // Encode even / uneven differently
+                    if (uneven)
+                    {
+                        int idx = imageBytes.Count - 1;
+                        imageBytes[idx] = (byte)(((color.R & 0xf0) >> 4) + (color.G & 0xf0));
+                        imageBytes.Add((byte)((color.B & 0xf0) >> 4));
+                        uneven = false;
+                    }
+                    else
+                    {
+                        int idx = imageBytes.Count - 1;
+                        imageBytes[idx] = (byte)(imageBytes[idx] + (byte)(color.R & 0xf0));
+                        imageBytes.Add((byte)(((color.G & 0xf0) >> 4) + (color.B & 0xf0)));
+                        imageBytes.Add(0);
+                        uneven = true;
+                    }
+                }
+            }
+            return imageBytes.ToArray();
+        }
+
+        public void ShowPixelArt(Color[][] colors)
+        {
+            var header = new byte[] { 0xbd, 0x00, 0x44, 0x00, 0x0a, 0x0a, 0x04 };
+
+            var imageData = ConvertColors(colors);
+
+            var payload = header.Append(imageData);
+
+            var message = BuildMessage(payload);
+
+            _client.GetStream().Write(message, 0, message.Length);
+
+            //data = data
+            //ck1, ck2 = checksum(sum(head) + sum(data))
+
+            //msg = [0x01] + head + mask(data) + mask([ck1, ck2]) + [0x02]
+            //return msg
+        }
+    }
+}
--- AlniTimeBoxMiniConsoleApp/Program.cs
+++ AlniTimeBoxMiniConsoleApp/Program.cs
@@ -1,15 +1,129 @@
-﻿using System;
+﻿using Knom.TimeBox;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AlniTimeBoxMiniConsoleApp
 {
     class Program
     {
+        public static readonly Color[][] COLOR_TEXT_TES = new Color[][]
+        {
+            //           | ----------------- T ----------------- |              | ----------------- E ----------------- |              | ----------------- S ----------------- |
+            new Color[] {Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Orange},
+            new Color[] {Color.Black,  Color.Orange, Color.Black,  Color.Black, Color.Orange, Color.Black,  Color.Black,  Color.Black, Color.Orange, Color.Black,  Color.Black},
+            new Color[] {Color.Black,  Color.Orange, Color.Black,  Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Orange},
+            new Color[] {Color.Black,  Color.Orange, Color.Black,  Color.Black, Color.Orange, Color.Black,  Color.Black,  Color.Black, Color.Black,  Color.Black,  Color.Orange},
+            new Color[] {Color.Black,  Color.Orange, Color.Black,  Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Orange},
+
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black}
+        };
+
+        public static readonly Color[][] COLOR_TEXT_HI = new Color[][]
+        {
+            //           | ----------------- H ----------------- |              | --- I --- |                           | --- ! --- |              | --- ! --- |
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black, Color.Black, Color.Orange, Color.Black, Color.Orange, Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black, Color.Black, Color.Orange, Color.Black, Color.Orange, Color.Black},
+            new Color[] {Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Black, Color.Black, Color.Orange, Color.Black, Color.Orange, Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black, Color.Black, Color.Black,  Color.Black, Color.Black,  Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black, Color.Black, Color.Orange, Color.Black, Color.Orange, Color.Black},
+
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black}
+        };
+
+
+        public static readonly Color[][] COLOR_TEXT_DAD = new Color[][]
+        {
+            //           | ----------------- D ----------------- |              | ----------------- A ----------------- | ¨            | ----------------- D ----------------- |
+            new Color[] {Color.Orange, Color.Orange,  Color.Black, Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange},
+            new Color[] {Color.Orange, Color.Orange,  Color.Black, Color.Black, Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Black},
+
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black}
+        };
+
+        public static readonly Color[][] COLOR_TEXT_MA = new Color[][]
+        {
+            //           | ------------------------------- M ------------------------------- |              | ----------------- A ----------------- |
+            new Color[] {Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Black, Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Black},
+            new Color[] {Color.Orange, Color.Black,  Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Orange, Color.Black,  Color.Orange, Color.Black, Color.Black},
+
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black, Color.Black}
+        };
+
+
+        public static readonly Color[][] COLOR_ART_PIZZA = new Color[][]
+        {
+            // Based on Pizza Emoji (scaled down to 11x11) from "twemoji-11.2.0" (16x16/1f355.png),
+
+            new Color[] {Color.Black, Color.Black,  Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Orange, Color.Yellow, Color.Yellow, Color.Yellow, Color.Orange, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Orange, Color.Yellow, Color.Red,    Color.Red,    Color.Yellow, Color.Yellow, Color.Yellow, Color.Orange, Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Orange, Color.Red,    Color.Red,    Color.Yellow, Color.Red,    Color.Red,    Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Orange, Color.Yellow, Color.Yellow, Color.Yellow, Color.Red,    Color.Red,    Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Red,    Color.Red,    Color.Orange, Color.Yellow, Color.Orange, Color.Black,  Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Red,    Color.Red,    Color.Orange, Color.Orange, Color.Orange, Color.Black,  Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Orange, Color.Yellow, Color.Red,    Color.Red,    Color.Black,  Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Orange, Color.Orange, Color.Red,    Color.Red,    Color.Black,  Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black,  Color.Orange, Color.Yellow, Color.Black,  Color.Black,  Color.Black,  Color.Black},
+            new Color[] {Color.Black, Color.Black,  Color.Black,  Color.Black,  Color.Black,  Color.Orange, Color.Black,  Color.Black,  Color.Black,  Color.Black,  Color.Black}
+        };
         static void Main(string[] args)
         {
+            AlniTimeBoxDevice box = new AlniTimeBoxDevice();
+
+            box.Connect();
+
+            //box.SetView(TimeBoxDevice.ViewType.Clock);
+            box.SetTimeColor(Color.Orange);
+            box.SetTime(DateTime.Now);
+
+            //box.ShowPixelArt(COLOR_TEXT_MA);
+            box.ShowPixelArt(COLOR_ART_PIZZA);
+
+            //box.SetTempUnit(fahrenheit: false);
+            //box.SetTempUnitAndColor(Color.Aqua, fahrenheit: false);
+
+            //box.SetVolume(0);
+            //box.SetVolume(16);
+
+            //box.ShowImage("C:\\Work\\projects\\Knom.TimeBox\\src\\SampleApp\\testdata\\squares.png");
+            //box.SetView(TimeBoxDevice.ViewType.Image);
+            //Thread.Sleep(1000);
+            //box.ShowImage("C:\\Users\\alexa\\Desktop\\1f355.png");
+
+            //box.AnimateImages("C:\\Work\\projects\\Knom.TimeBox\\src\\SampleApp\\testdata\\exp");
+
+            Thread.Sleep(Int32.MaxValue);
         }
     }
 }
